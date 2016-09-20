a3af9cc6ec94b13247a24b16bfec2ecbe490db56
ItsHoff <ItsHoff@users.noreply.github.com>
2016-09-20 21:43:45 +0300

    Improve emacs configuration

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -10,25 +10,104 @@
   (package-refresh-contents)
   (package-install 'use-package))
 
-;; Install all packages automatically
-(setq use-package-always-ensure t)
-
-;; PACKAGES
-(use-package evil
-  :init (evil-mode t))
-(use-package solarized-theme
-  :config (load-theme 'solarized-dark t))
-
 ;; GENERAL SETTINGS
-(setq visible-bell 1)              ; No error beep
-(tool-bar-mode -1)                 ; No toolbar
-(scroll-bar-mode -1)               ; No scrollbar
-(setq inhibit-start-up-message t)  ; No message at startup
-(global-linum-mode 1)              ; Show line numbers
+
+(setq visible-bell 1)               ; No error beep
+(tool-bar-mode -1)                  ; No toolbar
+(scroll-bar-mode -1)                ; No scrollbar
+(setq inhibit-startup-screen t)     ; No message at startup
+(global-linum-mode 1)               ; Show line numbers
+(show-paren-mode 1)                 ; Show matching parenthesis
+
+; Smooth scrolling
+(setq scroll-step 1)
+(setq scroll-margin 5)
+(setq scroll-conservatively 10000)
+(setq auto-window-vscroll nil)
+(setq hscroll-step 1)
+(setq hscroll-margin 5)
+
+
+;; BACKUPS ETC.
+
+; Put backups in .emacs.d
+(defvar backup-directory (concat user-emacs-directory "backups"))
+(if (not (file-exists-p backup-directory))
+        (make-directory backup-directory t))
+(setq backup-directory-alist `(("." . ,backup-directory)))
+
+(setq create-lockfiles nil)         ; Don't create lockfiles
+(setq auto-save-default nil)        ; No auto-saves
 
 ;; FONT
 (set-frame-font "Consolas-11")
 
 ;; TABS & SPACES
-(setq-default indent-tabs-mode nil) ; Use spaces instead of tabs
-(setq tab-width 4)                  ; Tab = 4 spaces
+(setq-default tab-always-indent nil)    ; Allow tabbing outside of indent
+(setq-default indent-tabs-mode nil)     ; Use spaces instead of tabs
+(setq-default tab-width 4)              ; Tab = 4 spaces
+
+;; HOOKS
+
+; Auto-save on focus lost
+(add-hook 'focus-out-hook (lambda () (interactive) (save-some-buffers t)))
+
+; Remove trailing whitespace before save
+(add-hook 'before-save-hook 'delete-trailing-whitespace)
+
+;; PACKAGES
+
+;; Install all packages automatically
+(setq use-package-always-ensure t)
+
+;; General
+(use-package company
+  :init (global-company-mode)
+  :config
+  (setq company-idle-delay 0.2)
+  (setq company-minimum-prefix-length 1))
+
+(use-package evil
+  :init (evil-mode t)
+  :config
+  (define-key evil-normal-state-map "H" 'evil-first-non-blank)
+  (define-key evil-normal-state-map "L" 'evil-end-of-line))
+
+(use-package flycheck
+  :init (global-flycheck-mode))
+
+(use-package spaceline-config
+  :ensure spaceline
+  :config
+  (setq powerline-default-separator "wave")
+  (setq spaceline-highlight-face-func 'spaceline-highlight-face-evil-state)
+  (spaceline-spacemacs-theme))
+
+(use-package solarized-theme
+  :config
+  (setq x-underline-at-descent-line t)
+  (load-theme 'solarized-dark t))
+
+;; Rust
+
+(use-package cargo
+  :config (add-hook 'rust-mode-hook 'cargo-minor-mode))
+
+(use-package flycheck-rust
+  :config (add-hook 'flycheck-mode-hook #'flycheck-rust-setup))
+
+(use-package racer
+  :init (setq racer-rust-src-path "C:\\Programming\\Rust-GNU\\src\\rustc-nightly\\src")
+  :config
+  (add-hook 'rust-mode-hook #'racer-mode)
+  (add-hook 'racer-mode-hook #'eldoc-mode))
+
+(use-package rust-mode
+  :mode "\\.rs\\'")
+
+;; Tex
+(use-package tex-site
+  :ensure auctex)
+
+(provide 'init)
+;;; init.el ends here

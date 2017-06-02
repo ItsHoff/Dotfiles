308471a11d3849ea48129c45438d0ea29f6d3679
ItsHoff <ItsHoff@users.noreply.github.com>
2017-06-02 10:49:32 +0300

    Add vundle installation to init scripts

--- install.sh
+++ install.sh
@@ -3,3 +3,4 @@ ln -s ~/Dotfiles/.vimrc ~/.vimrc
 ln -s ~/Dotfiles/.gitconfig ~/.gitconfig
 ln -s ~/Dotfiles/.tmux.conf ~/.tmux.conf
 ln -s ~/Dotfiles/.emacs.d ~/
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim

f171d0c975b1d94e6e4188f1d6af32dddb49f810
ItsHoff <ItsHoff@users.noreply.github.com>
2017-06-12 13:30:23 +0300

    Remove gitconfig

--- install.bat
+++ install.bat
@@ -1,4 +1,3 @@
 mklink %UserProfile%\.vimrc %UserProfile%\Dotfiles\.vimrc
-mklink %UserProfile%\.gitconfig %UserProfile%\Dotfiles\.gitconfig
 mklink /D %UserProfile%\.emacs.d %UserProfile%\Dotfiles\.emacs.d
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim

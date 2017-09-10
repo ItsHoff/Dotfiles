83ad3deeed2246d89dcb8f7f90225aca0e43984b
ItsHoff <ItsHoff@users.noreply.github.com>
2017-09-10 18:04:43 +0300

    Properly install vundle on windows

--- install.bat
+++ install.bat
@@ -1,4 +1,4 @@
 mklink %UserProfile%\.vimrc %UserProfile%\Dotfiles\.vimrc
 mklink %UserProfile%\AppData\Roaming\ConEmu.xml %UserProfile%\Dotfiles\ConEmu.xml
 mklink /D %UserProfile%\.emacs.d %UserProfile%\Dotfiles\.emacs.d
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+git clone https://github.com/VundleVim/Vundle.vim.git %UserProfile%\.vim\bundle\Vundle.vim

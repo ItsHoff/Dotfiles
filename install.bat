fe31b36b526394652ccecfe91c794b31d397ca24
ItsHoff <ItsHoff@users.noreply.github.com>
2018-01-02 16:44:25 +0200

    config: Make sure .gitconfig is properly linked

--- install.bat
+++ install.bat
@@ -1,4 +1,5 @@
 mklink %UserProfile%\.vimrc %UserProfile%\Dotfiles\.vimrc
+mklink %UserProfile%\.gitconfig %UserProfile%\Dotfiles\.gitconfig
 mklink %UserProfile%\AppData\Roaming\ConEmu.xml %UserProfile%\Dotfiles\ConEmu.xml
 mklink /D %UserProfile%\.emacs.d %UserProfile%\Dotfiles\.emacs.d
 git clone https://github.com/VundleVim/Vundle.vim.git %UserProfile%\.vim\bundle\Vundle.vim
--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #/bin/env sh
 ln -s ~/Dotfiles/.vimrc ~/.vimrc
+ln -s ~/Dotfiles/.gitconfig ~/.gitconfig
 ln -s ~/Dotfiles/.tmux.conf ~/.tmux.conf
 ln -s ~/Dotfiles/.emacs.d ~/
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim

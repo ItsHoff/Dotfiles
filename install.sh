1f61b453cddd1fde19f830f3d6549a95a03cd292
ItsHoff <ItsHoff@users.noreply.github.com>
2018-01-03 13:26:39 +0200

    config: Rename bashrc to profile and link it

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #/bin/env sh
 ln -s ~/Dotfiles/.vimrc ~/.vimrc
 ln -s ~/Dotfiles/.gitconfig ~/.gitconfig
+ln -s ~/Dotfiles/.bash_profile ~/.bash_profile
 ln -s ~/Dotfiles/.tmux.conf ~/.tmux.conf
 ln -s ~/Dotfiles/.emacs.d ~/
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim

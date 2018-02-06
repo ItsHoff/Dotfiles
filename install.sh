2d469490093593535d47c9f1df2c5cdf42856adb
ItsHoff <ItsHoff@users.noreply.github.com>
2018-02-06 10:44:58 +0200

    config: update bash profile installation

--- install.sh
+++ install.sh
@@ -1,7 +1,11 @@
 #/bin/env sh
 ln -s ~/Dotfiles/.vimrc ~/.vimrc
 ln -s ~/Dotfiles/.gitconfig ~/.gitconfig
-ln -s ~/Dotfiles/.bash_profile ~/.bash_profile
 ln -s ~/Dotfiles/.tmux.conf ~/.tmux.conf
 ln -s ~/Dotfiles/.emacs.d ~/
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+append_string="source ~/Dotfiles/.bash_profile"
+append_file="~/.bash_profile"
+if ! grep -qs "$append_string" "$append_file"; then
+  echo $append_string >> $append_file
+fi

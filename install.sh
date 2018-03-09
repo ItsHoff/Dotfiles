2975aad01e6c46eacfbdb644921fa2f7c5d47711
ItsHoff <ItsHoff@users.noreply.github.com>
2018-03-09 10:56:02 +0200

    config: improve install script

--- install.sh
+++ install.sh
@@ -3,9 +3,9 @@ ln -s ~/Dotfiles/.vimrc ~/.vimrc
 ln -s ~/Dotfiles/.gitconfig ~/.gitconfig
 ln -s ~/Dotfiles/.tmux.conf ~/.tmux.conf
 ln -s ~/Dotfiles/.emacs.d ~/
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-append_string="source ~/Dotfiles/.bash_profile"
-append_file="~/.bash_profile"
+append_string="source $HOME/Dotfiles/.bash_profile"
+append_file="$HOME/.bash_profile"
 if ! grep -qs "$append_string" "$append_file"; then
   echo $append_string >> $append_file
 fi
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim

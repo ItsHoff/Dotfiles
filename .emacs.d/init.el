2a6a84786d5f91d282a16283a83079dd98f83ea6
ItsHoff <ItsHoff@users.noreply.github.com>
2019-08-15 11:10:02 +0300

    Ensure display-line-numbers is loaded lazily

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -309,8 +309,9 @@
 
 ; Display line numbers
 (use-package display-line-numbers
+  :commands display-line-numbers-mode
   :custom (display-line-numbers-type 'visual)
-  :config (add-hook 'prog-mode-hook #'display-line-numbers-mode))
+  :init (add-hook 'prog-mode-hook #'display-line-numbers-mode))
 
 ; Shows documentation about symbol under point on the echo area
 (use-package eldoc

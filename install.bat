e36f15594234cb579edd10c64f9324a0c7e61aac
ItsHoff <ItsHoff@users.noreply.github.com>
2020-01-08 14:26:29 +0200

    Remove previous ConEmu config during install

--- install.bat
+++ install.bat
@@ -1,5 +1,6 @@
 mklink %UserProfile%\.vimrc %UserProfile%\Dotfiles\.vimrc
 mklink %UserProfile%\.gitconfig %UserProfile%\Dotfiles\.gitconfig
+del %UserProfile%\AppData\Roaming\ConEmu.xml
 mklink %UserProfile%\AppData\Roaming\ConEmu.xml %UserProfile%\Dotfiles\ConEmu.xml
 mklink /D %UserProfile%\.emacs.d %UserProfile%\Dotfiles\.emacs.d
 git clone https://github.com/VundleVim/Vundle.vim.git %UserProfile%\.vim\bundle\Vundle.vim

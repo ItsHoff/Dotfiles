de67b8b7e8207c542121123d4151f7b6b46b1431
ItsHoff <ItsHoff@users.noreply.github.com>
2021-08-06 10:06:55 +0300

    Improve local-util documentation

--- .emacs.d/init/local-util.el
+++ .emacs.d/init/local-util.el
@@ -1,27 +1,38 @@
 ;;; local-util --- local definition utilities -*- lexical-binding: t -*-
 ;;; Commentary:
+;;;
+;;; Example usage:
+;;;
+;;; local-conf.el
+;;; (require 'local-util)
+;;; (local/defvar ispell-program-name "c:/tools/msys64/mingw64/bin/hunspell.exe")
+;;;
+;;; init.el
+;;; (use-package ispell
+;;;   :config
+;;;   (local/custom ispell-program-name))
 
 ;;; Code:
 (defconst local/variable-prefix "local-value/" "Prefix for locally set values.")
 
 (defmacro local/defvar (variable value)
-  "Define local VALUE for VARIABLE."
+  "Define a local VALUE for a VARIABLE."
   `(defvar ,(intern (concat local/variable-prefix (symbol-name variable))) ,value))
 
 (defmacro local/setq (variable)
-  "Set VARIABLE to a local value if it has been defined."
+  "Set a VARIABLE to a local value if a local value has been defined."
   (let ((symbol (intern (concat local/variable-prefix (symbol-name variable)))))
     (when (boundp symbol)
       `(setq ,variable ,symbol))))
 
 (defmacro local/custom (variable)
-  "Set VARIABLE to a local value if it has been defined using custom."
+  "Set VARIABLE to a local value using custom if a local value has been defined."
   (let ((symbol (intern (concat local/variable-prefix (symbol-name variable)))))
     (when (boundp symbol)
       `(customize-set-variable ',variable ,(symbol-value symbol) "Customized by local-util"))))
 
 (defun local/setenv (variable value)
-  "Set environment variable VARIABLE to VALUE if it has not been set already."
+  "Set environment variable VARIABLE to VALUE if the environment variable has not been set already."
   (prin1 (getenv variable))
   (when (null (getenv variable))
       (setenv variable value)))

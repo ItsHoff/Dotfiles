d49f77a6362bb9b020eaa67ea4fbaa430ccd19e3
ItsHoff <ItsHoff@users.noreply.github.com>
2025-04-23 17:05:53 +0300

    Add elpaca lock file

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -35,14 +35,25 @@
 
 ;;; ELPACA -----------------------------------------------------------------------------------
 
-(defvar elpaca-installer-version 0.11)
 (defvar elpaca-directory (expand-file-name "elpaca/" user-emacs-directory))
+
+(defvar my/elpaca-lock-file (expand-file-name "elpaca.lock" elpaca-directory))
+;; Uncomment this and delete a package to revert the package to the version from the lock file.
+;; (setopt elpaca-lock-file my/elpaca-lock-file)
+
+(defun my/elpaca-write-lock-file ()
+  "Write an elpaca lock file to the path defined by `my/elpaca-lock-file`."
+  (interactive)
+  (elpaca-write-lock-file my/elpaca-lock-file))
+
+(defvar elpaca-installer-version 0.11)
 (defvar elpaca-builds-directory (expand-file-name "builds/" elpaca-directory))
 (defvar elpaca-repos-directory (expand-file-name "repos/" elpaca-directory))
 (defvar elpaca-order '(elpaca :repo "https://github.com/progfolio/elpaca.git"
                               :ref nil :depth 1 :inherit ignore
                               :files (:defaults "elpaca-test.el" (:exclude "extensions"))
                               :build (:not elpaca--activate-package)))
+
 (let* ((repo  (expand-file-name "elpaca/" elpaca-repos-directory))
        (build (expand-file-name "elpaca/" elpaca-builds-directory))
        (order (cdr elpaca-order))

c9b2384f3e4dc23504fcbf22fab39f3f208c7877
ItsHoff <ItsHoff@users.noreply.github.com>
2025-05-28 14:05:50 +0300

    Don't install Vundle

--- install.bat
+++ install.bat
@@ -3,4 +3,4 @@ mklink %UserProfile%\.gitconfig %UserProfile%\Dotfiles\.gitconfig
 del %UserProfile%\AppData\Roaming\ConEmu.xml
 mklink %UserProfile%\AppData\Roaming\ConEmu.xml %UserProfile%\Dotfiles\ConEmu.xml
 mklink /D %UserProfile%\.emacs.d %UserProfile%\Dotfiles\.emacs.d
-git clone https://github.com/VundleVim/Vundle.vim.git %UserProfile%\.vim\bundle\Vundle.vim
+rem git clone https://github.com/VundleVim/Vundle.vim.git %UserProfile%\.vim\bundle\Vundle.vim
--- install.sh
+++ install.sh
@@ -8,4 +8,4 @@ append_file="$HOME/.bash_profile"
 if ! grep -qs "$append_string" "$append_file"; then
   echo $append_string >> $append_file
 fi
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+# git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
